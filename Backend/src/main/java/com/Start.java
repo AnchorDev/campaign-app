506f50daaf2a90442eec7ad927b7e1aa78c45b17
Jakub Kotwica <01.jakub.kotwica@gmail.com>
2025-05-27 17:27:09 +0200

    cors

--- Backend/src/main/java/com/Start.java
+++ Backend/src/main/java/com/Start.java
@@ -2,12 +2,28 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @Validated
 public class Start {
-    public static  void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(Start.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://campaign-app-hz4y.vercel.app")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
